498237a4766281f6d02f807a8482bee21f3e289c
Sabir <sabirribas@gmail.com>
2013-08-20 11:33:38 -0300

    Removing socket communication of grader<->graderd (now it is non-blocking)

--- grader/quap/views.py
+++ grader/quap/views.py
@@ -2,6 +2,11 @@
 
 from django.http import HttpResponseRedirect, HttpResponse
 
+import sys,os
+path_graderd = os.path.dirname(os.path.abspath(__file__))+'/../../graderd'
+sys.path.append(path_graderd)
+import graderd
+
 def test(request):
 	content = "This is only a test!"
 	return HttpResponse( content )
@@ -43,7 +48,8 @@ def test_socket(request):
 	method,params = 'testcode',{'lang':'py','code':'a=int(raw_input(""))\nb=int(raw_input(""))\nprint a+b','tests':[('1\n2\n','3\n'),('2\n3\n','4\n')]}
 	method,params = 'testcode',{'lang':'cpp','code':"#include <iostream>\nusing namespace std;\nint main()\n{\n\tint a,b;\n\tcin >> a >> b;\n\tcout << a+b << endl;\n\treturn 0;\n}",'tests':[('1\n2\n','3\n'),('2\n3\n','4\n')]}
 
-	result = rpcclient.call(method,params)
+	#result = rpcclient.call(method,params)
+	result = graderd.testcode(params)
 
 	return HttpResponse( method + ' ' + str(params) + '<br/>' + str(result) )
 
@@ -67,7 +73,8 @@ def testcode(request):
 
 	method,params = 'testcode',{'lang':lang,'code':code,'tests':json.loads(tests)}
 
-	result = rpcclient.call(method,params)
+	#result = rpcclient.call(method,params)
+	result = graderd.testcode(params)
 
 	resultvalue = json.loads(result)
 
