072efd2acdfb3f01e06b48260cd91db16fb858ca
Guilherme Gomes <guilhermeki@yahoo.com.br>
2013-08-22 10:36:36 -0300

    Update lgcor.sh

--- examples/tester_logisim/lgcor.sh
+++ examples/tester_logisim/lgcor.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-#Exemplo de chamada: ./lgcor.sh -t tester.circ -s solution.circ -q query.txt
+#Exemplo de chamada: ./lgcor.sh -t tester.circ -s solution.circ -q query.circ -r RAM.txt
+#Obs: Bash é case sensitive
 
-LOGISIM="/usr/bin/logisim-generic-2.7.1-dac1.1.jar"
-RAM="RAM.txt"
-TEST_QUERY="QUERY.circ"
+LOGISIM="logisim-generic-2.7.1-dac1.1.jar"
 
-while getopts t:s:q:l: opt
+while getopts t:s:q:r: opt
 do
 	case $opt
 	in
 		t) TEST_CIRCUIT=${OPTARG};;
 		s) TEST_SOLUTION=${OPTARG};;
-		q) TEST_QUERY_TEXT=${OPTARG};;
+		q) TEST_QUERY=${OPTARG};;
+		r) RAM=${OPTARG};;
+		?) echo "Usage: ./lgcor.sh -t tester.circ -s solution.circ -q query.circ -r ram.txt"
+		   exit;;
 	esac
 done
 
-if [ -e $TEST_QUERY ]; then
-	rm $TEST_QUERY
-fi
-
-while read line
-do
-	echo "$line" >> "$TEST_QUERY"
-done < "$TEST_QUERY_TEXT"
-
-
+#Gera a saida base para comparação e a saida para o circuito sendo testado
 SOLUTION="java -jar $LOGISIM $TEST_CIRCUIT -tty table -load $RAM" 
 ${SOLUTION} > _aux1.txt
 sed '2d' _aux1.txt > sol_output.txt
@@ -34,31 +27,26 @@ ${TEST} > _aux2.txt
 sed '2d' _aux2.txt > test_output.txt
 
 
+#Compara os dois arquivos e imprime porcentagem de acertos
 if `diff sol_output.txt test_output.txt >/dev/null` && [ -s sol_output.txt ] && [ -s test_output.txt ]; then
-	echo "TRUE"
-	echo "GRADE:1"
+	echo "GRADE:1" 		#Circuito functiona corretamente
 else
-	echo "FALSE"
 	sed -n '8p' < sol_output.txt
 	diff -u sol_output.txt test_output.txt
 	diff -u sol_output.txt test_output.txt > result.txt
 
-	count=$(wc -l < sol_output.txt) #Number of inputs
-	err=$(grep -c '+' result.txt) #Number of errors
+	count=$(wc -l < sol_output.txt)
+	err=$(grep -c '+' result.txt)
 
-	echo "C: $count E: $err"
-	
-	let "err -= 2"
-	let "count -= 9"
-	echo "C: $count E: $err"
+	err=$((err -=2))
+	count=$((count -= 9))
 	res=$(echo "scale=2; 1 - $err/$count" | bc -l)
-	echo "GRADE:$res"
-	rm result.txt
+	echo "GRADE:$res" 	#Circuito não funciona corretamente
+	rm -f result.txt
 fi
 
-
-rm _aux1.txt
-rm _aux2.txt
-rm sol_output.txt
-rm test_output.txt
-rm $TEST_QUERY
+#Remove arquivos temporários gerados durante a execução
+rm -f _aux1.txt
+rm -f _aux2.txt
+rm -f sol_output.txt
+rm -f test_output.txt
